e1608d2e2ea8c7d07b58ea5c5a5ed697053be932
Robert McLaws (Microsoft MVP) <robert@advancedrei.com>
2017-06-10 01:45:06 -0400

    Adding package icons. Adding symbols build. Adding target frameworks. Adding WebAPI helpers.

--- DeployNuGet.bat
+++ DeployNuGet.bat
@@ -2,12 +2,10 @@
 echo Would you like to push the packages to NuGet when finished?
 set /p choice="Enter y/n: "
 
-set version=1.0.0-beta2c
-
-del builds\nuget\*.nupkg
 @echo on
-".nuget\nuget.exe" pack src\AdvancedREI.Testier.Restier\AdvancedREI.Testier.Restier.nuspec -OutputDirectory builds\nuget\ -version %version% -symbols
 if /i %choice% equ y (
-    ".nuget/nuget.exe" push builds\nuget\AdvancedREI.Testier.Restier.*.nupkg -Source https://www.nuget.org/api/v2/package
+    ".nuget/nuget.exe" push src\AdvancedREI.Breakdance\bin\debug\AdvancedREI.Breakdance.*.nupkg -Source https://www.nuget.org/api/v2/package
+    ".nuget/nuget.exe" push src\AdvancedREI.Breakdance.WebApi\bin\debug\AdvancedREI.Breakdance.WebApi.*.nupkg -Source https://www.nuget.org/api/v2/package
+    ".nuget/nuget.exe" push src\AdvancedREI.Breakdance.Restier\bin\debug\AdvancedREI.Breakdance.Restier.*.nupkg -Source https://www.nuget.org/api/v2/package
 )
 pause
---
+++ src/AdvancedREI.Breakdance.WebApi/WebApiTestHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace AdvancedREI.Breakdance.WebApi
+{
+    public static class WebApiTestHelpers
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="routeName"></param>
+        /// <param name="routePrefix"></param>
+        /// <returns></returns>
+        public static HttpConfiguration GetTestableConfiguration()
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            return config;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static HttpServer GetTestableServer()
+        {
+            return GetTestableConfiguration().GetTestableServer();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static HttpServer GetTestableServer(this HttpConfiguration config)
+        {
+            return new HttpServer(config)
+            {
+                InnerHandler = new HttpClientHandler
+                {
+                    AllowAutoRedirect = true
+                }
+            };
+        }
+
+    }
+
+}
