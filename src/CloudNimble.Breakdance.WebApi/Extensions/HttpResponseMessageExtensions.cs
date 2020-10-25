3f9b92f10caef75f598950cdf2f641e66cebeebd
Robert McLaws <robert@nimbleapps.cloud>
2020-10-25 11:33:08 -0400

    - Add HttpResponseMessageExtensions overloads to allow for passing in serializer settings.

--- src/CloudNimble.Breakdance.WebApi/Extensions/HttpResponseMessageExtensions.cs
+++ src/CloudNimble.Breakdance.WebApi/Extensions/HttpResponseMessageExtensions.cs
@@ -34,6 +34,28 @@ namespace System.Net.Http
         /// <summary>
         /// 
         /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public static async Task<(T Response, string ErrorContent)> DeserializeResponseAsync<T>(this HttpResponseMessage message, JsonSerializerSettings settings)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            if (!message.IsSuccessStatusCode)
+            {
+                return (default, await message.Content.ReadAsStringAsync().ConfigureAwait(false));
+            }
+            var content = await message.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return (JsonConvert.DeserializeObject<T>(content, settings), null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <typeparam name="TResponse"></typeparam>
         /// <typeparam name="TError"></typeparam>
         /// <param name="message"></param>
@@ -54,5 +76,30 @@ namespace System.Net.Http
             return (JsonConvert.DeserializeObject<TResponse>(content), default);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TResponse"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public static async Task<(TResponse Response, TError ErrorContent)> DeserializeResponseAsync<TResponse, TError>(this HttpResponseMessage message, JsonSerializerSettings settings)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            var content = await message.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            if (!message.IsSuccessStatusCode)
+            {
+                return (default, JsonConvert.DeserializeObject<TError>(content));
+            }
+            return (JsonConvert.DeserializeObject<TResponse>(content, settings), default);
+        }
+
     }
+
 }
