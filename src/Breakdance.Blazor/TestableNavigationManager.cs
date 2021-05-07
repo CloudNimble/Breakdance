8710885e669b7fd4acef1d7086e9e3f4e9dcb70b
Robert McLaws <robert@nimbleapps.cloud>
2021-05-07 01:41:39 -0400

    - First pass at a Blazor version.

---
+++ src/Breakdance.Blazor/TestableNavigationManager.cs
@@ -0,0 +1,71 @@
+﻿using Flurl;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CloudNimble.Breakdance.Blazor
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// Inspired by https://github.com/bUnit-dev/bUnit/issues/73#issuecomment-597828532
+    /// </remarks>
+    public class TestableNavigationManager : NavigationManager
+    {
+
+        #region Private Members
+
+        private string _baseUrl;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string NavigationResult { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SuppressMessage("Design", "CA1054:URI-like parameters should not be strings", Justification = "<Pending>")]
+        public TestableNavigationManager(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+            EnsureInitialized();
+        }
+
+        #endregion
+
+        #region Internal Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected sealed override void EnsureInitialized()
+        {
+            Initialize(_baseUrl, _baseUrl);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="forceLoad"></param>
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            NavigationResult = uri;
+            Uri = _baseUrl.AppendPathSegment(uri);
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/Breakdance.Tests.Blazor/TestableNavigationManagerTests.cs
@@ -0,0 +1,29 @@
+﻿using CloudNimble.Breakdance.Blazor;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Breakdance.Tests.Blazor
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestClass]
+    public class TestableNavigationManagerTests
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var manager = new TestableNavigationManager("https://localhost/");
+            manager.Uri.Should().Be("https://localhost/");
+            manager.NavigateTo("test");
+            manager.Uri.Should().Be("https://localhost/test");
+        }
+
+    }
+
+}
