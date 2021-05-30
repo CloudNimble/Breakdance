b1fc1f32cc52eacb5c793b5fd7320d63a1ff8c60
Robert McLaws <robert@nimbleapps.cloud>
2021-05-30 10:25:35 -0400

    - Make sure the IConfiguration from the TestHost is also registered in the BUnitTestContext. - Add DI container failover from GetService & GetServices.

--- src/CloudNimble.Breakdance.Tests.Blazor/BlazorTestBase_CoreTests.cs
+++ src/CloudNimble.Breakdance.Tests.Blazor/BlazorTestBase_CoreTests.cs
@@ -2,6 +2,7 @@
 using CloudNimble.Breakdance.Blazor;
 using FluentAssertions;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -22,31 +23,41 @@ namespace Breakdance.Tests.Blazor
         public void BlazorTestBase_Setup_CreatesTestContext_NoServices()
         {
             //RWM: We're not *quite* setting this up properly, because we want to test the state both before and after calling TestSetup();
+            TestHost.Should().BeNull();
             BUnitTestContext.Should().BeNull();
             RegisterServices.Should().BeNull();
+
             TestSetup();
+
+            TestHost.Should().NotBeNull();
             BUnitTestContext.Should().NotBeNull();
             RegisterServices.Should().BeNull();
-            BUnitTestContext.Services.Should().HaveCount(12);
+            TestHost.Services.GetAllServiceDescriptors().Should().HaveCount(32);
+            BUnitTestContext.Services.Should().HaveCount(13);
             GetService<NavigationManager>().Should().NotBeNull().And.BeOfType(typeof(FakeNavigationManager));
+            BUnitTestContext.Services.GetService<IConfiguration>().Should().NotBeNull();
         }
 
         /// <summary>
         /// Tests whether or not a <see cref="Bunit.TestContext"/> is created on setup. />
         /// </summary>
         [TestMethod]
-        public void BlazorTestBase_Setup_CreatesTestContext_ConflictingServices()
+        public void BlazorTestBase_TestSetup_CreatesTestContext_ConflictingServices()
         {
             //RWM: We're not *quite* setting this up properly, because we want to test the state both before and after calling TestSetup();
+            TestHost.Should().BeNull();
             BUnitTestContext.Should().BeNull();
             RegisterServices.Should().BeNull();
+
             RegisterServices = services => {
                 services.AddScoped<TestableNavigationManager>();
             };
             TestSetup();
+
+            TestHost.Should().NotBeNull();
             BUnitTestContext.Should().NotBeNull();
             RegisterServices.Should().NotBeNull();
-            BUnitTestContext.Services.Should().HaveCount(13);
+            BUnitTestContext.Services.Should().HaveCount(14);
             GetService<NavigationManager>().Should().NotBeNull().And.BeOfType(typeof(FakeNavigationManager));
             GetServices<NavigationManager>().Should().HaveCount(1);
             GetService<TestableNavigationManager>().Should().NotBeNull();
@@ -56,21 +67,54 @@ namespace Breakdance.Tests.Blazor
         /// Tests whether or not a <see cref="Bunit.TestContext"/> is created on setup. />
         /// </summary>
         [TestMethod]
-        public void BlazorTestBase_Setup_CreatesTestContext_Services()
+        public void BlazorTestBase_TestSetup_CreatesTestContext_Services()
         {
             //RWM: We're not *quite* setting this up properly, because we want to test the state both before and after calling TestSetup();
+            TestHost.Should().BeNull();
             BUnitTestContext.Should().BeNull();
             RegisterServices.Should().BeNull();
+
             RegisterServices = services => {
                 services.AddScoped<NavigationManager>(sp => new TestableNavigationManager());
             };
             TestSetup();
+
+            TestHost.Should().NotBeNull();
             BUnitTestContext.Should().NotBeNull();
             RegisterServices.Should().NotBeNull();
-            BUnitTestContext.Services.Should().HaveCount(13);
+            BUnitTestContext.Services.Should().HaveCount(14);
             GetService<NavigationManager>().Should().NotBeNull().And.BeOfType(typeof(TestableNavigationManager));
             GetServices<NavigationManager>().Should().HaveCount(2);
         }
+
+        /// <summary>
+        /// Tests whether or not a <see cref="Bunit.TestContext"/> is created on setup. />
+        /// </summary>
+        [TestMethod]
+        public void BlazorTestBase_GetServices_ReturnsServiceOnlyInTestContext()
+        {
+            //RWM: We're not *quite* setting this up properly, because we want to test the state both before and after calling TestSetup();
+            TestHost.Should().BeNull();
+            BUnitTestContext.Should().BeNull();
+            RegisterServices.Should().BeNull();
+
+            TestHostBuilder.ConfigureServices((context, services) => services.AddSingleton<DummyService>());
+            TestSetup();
+
+            TestHost.Should().NotBeNull();
+            BUnitTestContext.Should().NotBeNull();
+            RegisterServices.Should().BeNull();
+            BUnitTestContext.Services.Should().HaveCount(13);
+            BUnitTestContext.Services.GetService<DummyService>().Should().BeNull();
+            TestHost.Services.GetAllServiceDescriptors().Should().HaveCount(33);
+            TestHost.Services.GetService<DummyService>().Should().NotBeNull();
+            GetService<DummyService>().Should().NotBeNull();
+        }
+
+    }
+
+    public class DummyService
+    {
 
     }
 
