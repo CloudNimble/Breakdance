1687956b048997a36a2cb1588fb643ef465b9763
Robert McLaws <robert@nimbleapps.cloud>
2021-06-22 01:30:37 -0400

    - Minor code cleanup

--- src/CloudNimble.Breakdance.AspNetCore/AspNetCoreTestHelpers.cs
+++ src/CloudNimble.Breakdance.AspNetCore/AspNetCoreTestHelpers.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.TestHost;
-using System.Net.Http;
-using CloudNimble.Breakdance.Assemblies;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CloudNimble.Breakdance.AspNetCore
 {
