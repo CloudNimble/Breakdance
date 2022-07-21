1a8adcda802dbd2b9a4bc6ce610095fbb7edd91a
James Caldwell <james.caldwell@nimbleapps.cloud>
2022-07-21 09:43:31 -0400

    Initial setup of TestableHubConnection including some simple tests.

---
+++ src/CloudNimble.Breakdance.Tests.AspNetCore.SignalR/ClientConfigTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudNimble.Breakdance.Tests.AspNetCore.SignalR
+{
+
+    /// <summary>
+    /// Tests the configuration for clients using the <see cref="TestableHubConnection"/>.
+    /// </summary>
+    [TestClass]
+    public class ClientConfigTests : ClientTestBase
+    {
+
+        /// <summary>
+        /// Sets up services needed for tests.
+        /// </summary>
+        [TestInitialize]
+        public void Setup() => TestSetup();
+
+        /// <summary>
+        /// Tests that the <see cref="IServiceProvider"/> contains a valid <see cref="TestableHubConnection"/>.
+        /// </summary>
+        [TestMethod]
+        public async Task TestHost_CanConfigure_TestableHubConnection()
+        {
+            var hubConnection = GetService<HubConnection>();
+            hubConnection.Should().NotBeNull();
+            hubConnection.State.Should().Be(HubConnectionState.Disconnected);
+            await hubConnection.StartAsync();
+            //JHC Note: can't assert this test because our class does not change the State value
+            //hubConnection.State.Should().Be(HubConnectionState.Connected);
+        }
+
+    }
+
+}
